c98ba6b26f48e21dce5af10a0eeb7b3e3892414a
Rakshit Negi <122672621+rakshitnegi1234@users.noreply.github.com>
2024-08-01 23:54:14 +0530

    Create q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,32 @@
+Write a C program that takes a string input from the user and reverses the string.
+
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char *str) {
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    reverseString(str);
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
+
